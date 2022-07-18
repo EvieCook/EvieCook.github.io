cb57d74a28aefed836224f9f8f9a09521b20d23d
Evie Cook <63733072+EvieCook@users.noreply.github.com>
2022-07-17 19:05:28 -0600

    Adding home page photo

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Pages.css'
 import ContactButton from '../components/ContactButton/contactButton'
-import defaultImage from '../images/defaultImage.png'
+import clearPhoto from '../images/defaultImage.png'
 import CaptionedImages from '../components/CaptionedImage/CaptionedImage'
 
 const HomePage = () => {
@@ -9,7 +9,7 @@ const HomePage = () => {
         <>
         <h1>Evelyn Cook</h1>
         <p>I am a mechanical engineer with a passion for sharing my love of Science, Technology, Engineering, and Math with younger generations. I value hardwork, dedication, and devotion to this ever growing field.  </p>
-        <CaptionedImages image={defaultImage} alt="Portrait of Evelyn Cook" maxHeight={72}/>
+        <CaptionedImages image={clearPhoto} alt="Portrait of Evelyn Cook" maxHeight={72}/>
 
         <br></br>
         <h1>Contact</h1>
