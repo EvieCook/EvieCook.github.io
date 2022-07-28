4336f42fd118767939fd6c04541811ce743f3c05
Evie Cook <63733072+EvieCook@users.noreply.github.com>
2022-07-27 22:02:20 -0600

    Updated Project Photos

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,38 +1,42 @@
-import defaultImage from '../images/defaultImage.png'
+import SWELogo from '../images/SWELogo.jpg'
+import GSEDLogo from '../images/GSEDLogo.png'
+import GaSLogo2 from '../images/GaSLogo2.jpg'
+import capstoneLogo from '../images/capstoneLogo.jpg'
+import vanguardLogo from '../images/vanguardLogo.jpeg'
 
 const allProjects = [
   {
     title: 'Girl Scout Engineering Day',
     description: 'Example Description',
-    image: defaultImage,
+    image: GSEDLogo,
     link: 'girlscoutengineeringday'
   },
 
   {
     title: 'Girls Are S.M.A.R.T.',
     description: 'Example Description',
-    image: defaultImage,
+    image: GaSLogo2,
     link: 'girlsaresmart'
   },
 
   {
     title: 'Society of Women Engineers',
     description: 'Example Description',
-    image: defaultImage,
+    image: SWELogo,
     link: 'societyofwomenengineers'
   },
 
   {
     title: 'Vanguard Scholars Program',
     description: 'Example Description',
-    image: defaultImage,
+    image: vanguardLogo,
     link: 'vanguardscholarsprogram'
   },
 
   {
     title: 'Senior Capstone',
     description: 'Example Description',
-    image: defaultImage,
+    image: capstoneLogo,
     link: 'seniorcapstone'
   }
 ]
